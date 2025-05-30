45b8dd65ef9eb4b9664a9590c1a73366b33123ad
TrapTheOnly <63204619+TrapTheOnly@users.noreply.github.com>
2025-05-31 00:18:12 +0400

    Update application namespace and versioning in build.gradle.kts. Change namespace to 'com.gico.calorium', update applicationId, and set versionCode to 2 with versionName '1.1.0'.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.calorie_tracker"
+    namespace = "com.gico.calorium"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,14 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.calorie_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.gico.calorium"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 2
+        versionName = "1.1.0"  // New version name
     }
 
     buildTypes {
---
+++ android/app/src/main/kotlin/com/gico/calorium/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gico.calorium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() 
